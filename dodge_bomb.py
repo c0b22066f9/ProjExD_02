a332337a71173bfd72bf290750952321d148a035
Yuki Saito <c0b22066f9@edu.teu.ac.jp>
2023-06-20 14:28:29 +0900

    第1問目

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,3 +1,4 @@
+import random
 import sys
 import pygame as pg
 
@@ -11,8 +12,17 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    bd_img = pg.Surface((20, 20))
+    bd_img.set_colorkey((0, 0, 0))
+    pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
+    x = random.randint(0, WIDTH)
+    y = random.randint(0, HEIGHT)
+    bombrect = bd_img.get_rect()
+    bombrect.center = x, y
+    vx, vy = +5, +5
     clock = pg.time.Clock()
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -20,6 +30,8 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        bombrect.move_ip(vx, vy)
+        screen.blit(bd_img, bombrect)
         pg.display.update()
         tmr += 1
         clock.tick(10)
