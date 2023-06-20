cb57555678a19fce42b25533ef63d18a927d6821
Yuki Saito <c0b22066f9@edu.teu.ac.jp>
2023-06-20 15:30:55 +0900

    第5問目

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -11,6 +11,13 @@ command = {
     pg.K_RIGHT: (+5, 0),
 }
 
+def check_bound(rect: pg.Rect) -> tuple[bool, bool]:
+    yoko, tate = True, True
+    if rect.left < 0 or WIDTH < rect.right:
+        yoko = False
+    if rect.top < 0 or HEIGHT < rect.bottom:
+        tate = False
+    return yoko, tate
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -34,6 +41,10 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        
+        if kk_rct.colliderect(bombrect):
+            print("ゲームオーバー")
+            return 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for k, mv in command.items():
@@ -41,10 +52,18 @@ def main():
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rct)
         bombrect.move_ip(vx, vy)
+        yoko, tate = check_bound(bombrect)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         screen.blit(bd_img, bombrect)
+
         pg.display.update()
         tmr += 1
         clock.tick(50)
@@ -55,3 +74,4 @@ if __name__ == "__main__":
     main()
     pg.quit()
     sys.exit()
+
