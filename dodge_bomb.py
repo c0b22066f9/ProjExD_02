fbf2a1b8189b3cbd291b684274717865f060a437
Yuki Saito <c0b22066f9@edu.teu.ac.jp>
2023-06-20 14:50:36 +0900

    第3問目

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,7 +4,12 @@ import pygame as pg
 
 
 WIDTH, HEIGHT = 1600, 900
-
+command = {
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
+}
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -18,6 +23,8 @@ def main():
     x = random.randint(0, WIDTH)
     y = random.randint(0, HEIGHT)
     bombrect = bd_img.get_rect()
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 900, 400
     bombrect.center = x, y
     vx, vy = +5, +5
     clock = pg.time.Clock()
@@ -27,9 +34,15 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0, 0]
+        for k, mv in command.items():
+            if key_lst[k]:
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+        kk_rct.move_ip(sum_mv)
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        screen.blit(kk_img, kk_rct)
         bombrect.move_ip(vx, vy)
         screen.blit(bd_img, bombrect)
         pg.display.update()
